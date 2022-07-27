85b74cc91519db50ee24d180cf07f550906a0da1
CarterSnich <jmestreraclosa@gmail.com>
2022-07-27 23:38:08 +0800

    fixed composer dependencies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Administrator;
 use App\Models\Application;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
